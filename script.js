5e5eb0e27931ee8e34fb5904985a0417053adcf3
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-03-02 19:44:34 +0530

    js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function show(){
-    document.getElementById("p1").innerHTML = "Thank you for clicking the button!!!";
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElasped = currentTime - startTime;
+        var run = ease(timeElasped, startPosition, distance, duration);
+        window.scrollTo(0,run);
+        if(timeElasped < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    };
+
+    requestAnimationFrame(animation);
 }
+
+var section1 = document.querySelector('.section1');
+section1.addEventListener('click', function(){
+    smoothScroll(".section2", 1000)
+});
+var section2 = document.querySelector('.section2');
+section2.addEventListener('click', function(){
+    smoothScroll(".section1", 1000)
+});
+
