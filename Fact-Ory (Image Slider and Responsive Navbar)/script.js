31d7c75e8389ddee47aedf5e5f9c73048d6d2b8c
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-03-07 00:05:39 +0530

    Refine the code

--- Fact-Ory (Image Slider and Responsive Navbar)/script.js
+++ Fact-Ory (Image Slider and Responsive Navbar)/script.js
@@ -49,7 +49,7 @@ const navSlide = () => {
     burger.addEventListener('click', ()=>{
         nav.classList.toggle("nav-active");
         burger.classList.toggle("toggle");
-        carouselContainer.style.marginTop = "92vh";
+        carouselContainer.classList.toggle("carousel-update");
         navLinks.forEach((link, index)=>{
             if(link.style.animation) link.style.animation = '';
             else link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
