5323e59f4f5dcab4d5143780cd763f6ff81f5797
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-03-06 23:50:16 +0530

    written javascript

--- Fact-Ory (Image Slider Component)/script.js
+++ Fact-Ory (Image Slider Component)/script.js
@@ -40,3 +40,21 @@ window.addEventListener('resize', () => {
     size = carouselImage[0].clientWidth;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
+
+const navSlide = () => {
+    const carouselContainer = document.querySelector(".carousel-container");
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle("nav-active");
+        burger.classList.toggle("toggle");
+        carouselContainer.style.marginTop = "92vh";
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation) link.style.animation = '';
+            else link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
+        });
+    });
+}
+
+navSlide()
