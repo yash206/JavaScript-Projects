39141d4b50747e79bccc6de798fcd665b02eb39b
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-03-09 01:36:25 +0530

    made changes

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,6 +1,9 @@
-window.addEventListener("load", ()=>{
-    let long,lat;
-    if(navigator.geolocation){
+window.addEventListener("load", () => {
+    let long, lat;
+    const temperatureUnit = document.querySelector(".temperature-unit");
+    const temperatureDegree = document.querySelector(".temperature-degree");
+    const degree = document.querySelector(".degree");
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
@@ -12,40 +15,25 @@ window.addEventListener("load", ()=>{
                     document.querySelector(".temperature-degree").textContent = Math.round(data.main.temp - 273.15);
                     document.querySelector(".temperature-description").textContent = data.weather[0].description.toUpperCase();
                     document.querySelector(".location-timezone").textContent = data.name;
-                    console.log(data.weather[0].icon);
-                    // document.querySelector(".location-icon").innerHTML = data.weather[0].icon;
-                    console.log(data)
+                    document.querySelector(".location-icon").innerHTML = `<img src = "images/${data.weather[0].icon}.png"/>`;
+
+                    let initial_temperature = temperatureDegree.textContent;
+                    degree.addEventListener("click", () => {
+                        if (temperatureUnit.textContent === '\u00B0F') {
+                            initial_temperature = [(initial_temperature - 32) * 5] / 9;
+                            temperatureDegree.innerHTML = Math.round(initial_temperature);
+                            temperatureUnit.innerHTML = '\u00B0C'
+                        }
+                        else if(temperatureUnit.textContent === '\u00B0C') {
+                            initial_temperature = (initial_temperature * (9 / 5)) + 32
+                            temperatureDegree.innerHTML = Math.round(initial_temperature);
+                            temperatureUnit.innerHTML = '\u00B0F'
+                        }
+                    });
                 })
-                
+
         })
-    }else{
+    } else {
         document.querySelector(".location-timezone").innerHTML = 'Please allow location permission <br> for fetching your location!!!';
     }
-    // function setIcons(icon, iconID){
-    //     const skycons = new skycons({color : 'white'});
-    // }
 });
-
-
-
-function togg(){
-const temperatureUnit = document.querySelector(".temperature-unit");
-const temperatureDegree = document.querySelector(".temperature-degree");
-const degree = document.querySelector(".degree");
-let initial_temperature = temperatureDegree.textContent;
-console.log(initial_temperature);
-degree.addEventListener("click", ()=>{
-    if(temperatureUnit.textContent == '&deg;F'){
-        initial_temperature = [(initial_temperature - 32) * 5] / 9;
-        temperatureDegree.innerHTML = Math.round(initial_temperature);
-        temperatureUnit.innerHTML = '&deg;C'
-    }
-    else{
-        initial_temperature = (initial_temperature * (9/5)) + 32
-        temperatureDegree.innerHTML = Math.round(initial_temperature);
-        temperatureUnit.innerHTML = '&deg;F'
-    }
-});
-}
-
-togg()
